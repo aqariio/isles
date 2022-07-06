7198e59c56fc6941b5c5f334cb31bad835028e25
aqariio <aquario.mw@gmail.com>
2022-07-06 19:11:52 -0400

    Initial commit

---
+++ src/main/java/aqario/kilns/Kilns.java
@@ -0,0 +1,14 @@
+package aqario.kilns;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kilns implements ModInitializer {
+      public static final String MODID = "kilns";
+      public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/aqario/kilns/KilnsClient.java
@@ -0,0 +1,10 @@
+package aqario.kilns;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KilnsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
